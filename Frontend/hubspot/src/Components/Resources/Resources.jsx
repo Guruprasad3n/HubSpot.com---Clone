4e7bfe27ebee2849ed054052f05d8dcf691b553d
Guruprasad3n <mr.guruprasadb@gmail.com>
2022-11-09 15:07:30 +0530

    U5 Project

--- Frontend/hubspot/src/Components/Resources/Resources.jsx
+++ Frontend/hubspot/src/Components/Resources/Resources.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Recource() {
+function Resources() {
   return (
-    <div>Recource</div>
+    <div>Resources</div>
   )
 }
 
-export default Recource
+export default Resources
