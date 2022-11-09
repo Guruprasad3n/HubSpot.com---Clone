0e5808ef7227f926e7e441eff6c3469bdc0c2883
Guruprasad3n <mr.guruprasadb@gmail.com>
2022-11-09 20:28:16 +0530

    Day-2

--- Frontend/hubspot/src/Components/Resources/Resources.jsx
+++ Frontend/hubspot/src/Components/Resources/Resources.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React from "react";
+import "./resources.css";
 
 function Resources() {
   return (
-    <div>Resources</div>
-  )
+    <div>
+      <div className="mainContainer">
+        <div className="topResource">
+          {/* Flex Div-1 */}
+
+          <div className="flexDiv1">
+            <img className="resourceTopImage"
+              src="https://blog.hubspot.com/hubfs/marketing-plan.png"
+              alt="Resources Top Image"
+            />
+
+            <p className="ResourceTopHeading">
+              5 Steps to Create an Outstanding Marketing Plan [Free Template]
+            </p>
+            <p className="ResourceTopSubHeading">
+              <span className="ResourceTopSubHeadingUnderLine" >MARKETING |</span> 20 MIN READ{" "}
+            </p>
+          </div>
+
+          {/* Flex Div-2 */}
+
+          <div className="flexDiv2">
+            <div className="flexDiv2SideHeading">
+              <p className="flexDiv2SideHeadingText">Most Popular</p>
+            </div>
+            <div className="flexRightTextDivs">
+              <p className="lexRightTextDivs1">
+                How to Create Detailed Buyer Persons for Your Businedd [Free
+                Persona Template]
+              </p>
+              <p className="lexRightTextDivs2">
+                <span className="lexRightTextDivsType">MATKETING |</span> 14 MIN READ{" "}
+              </p>
+            </div>
+
+            <div className="flexRightTextDivs">
+              <p className="lexRightTextDivs1">
+                How to Create Create a Sales Plan: Template + Examples
+              </p>
+              <p className="lexRightTextDivs2">
+                <span className="lexRightTextDivsType">SALES |</span> 18 MIN READ{" "}
+              </p>
+            </div>
+
+
+            <div className="flexRightTextDivs">
+              <p className="lexRightTextDivs1">
+                13 New Customers Onboarding Best Practices
+              </p>
+              <p className="lexRightTextDivs2">
+                <span className="lexRightTextDivsType">SERVICE |</span> 9 MIN READ{" "}
+              </p>
+            </div>
+{/* //////////////// */}
+
+
+
+<div className="flexDiv2SideHeading">
+              <p className="flexDiv2SideHeadingText">Editor's Picks</p>
+            </div>
+            <div className="flexRightTextDivs">
+              <p className="lexRightTextDivs1">
+                The HubSpot Blog's 2022 Content & Media Strategy Report [Data]
+              </p>
+              <p className="lexRightTextDivs2">
+                <span className="lexRightTextDivsType">MATKETING |</span> 17 MIN READ{" "}
+              </p>
+            </div>
+
+            <div className="flexRightTextDivs">
+              <p className="lexRightTextDivs1">
+                17 Best Sales Strategies, Plans, & Initiatives for Success [Templates]
+              </p>
+              <p className="lexRightTextDivs2">
+                <span className="lexRightTextDivsType">SALES |</span> 14 MIN READ{" "}
+              </p>
+            </div>
+
+
+            <div className="flexRightTextDivs">
+              <p className="lexRightTextDivs1">
+                The 15 Customer Success Metrics That Actually Matter
+              </p>
+              <p className="lexRightTextDivs2">
+                <span className="lexRightTextDivsType">SERVICE |</span> 15 MIN READ{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+{/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+
+<div className="SecondContainer">
+
+
+
+  sdfffffffffffffffffffffffffff
+</div>
+
+
+
+
+
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Resources
+export default Resources;
