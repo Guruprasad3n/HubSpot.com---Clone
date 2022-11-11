3575f4842222524153f91de9b256d6f426f754fd
Rohit Singh <kailash.jc.rs@gmail.com>
2022-11-11 10:17:19 +0530

    Navbar & Footer

--- Frontend/hubspot/src/index.js
+++ Frontend/hubspot/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
 
