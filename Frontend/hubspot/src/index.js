507049be5886459b4cbf70e0003f73044fb53291
Sarsande-Sunil <priyankaingle250@gmail.com>
2022-11-12 17:07:17 +0530

    added-sucessfully

--- Frontend/hubspot/src/index.js
+++ Frontend/hubspot/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { ChakraProvider } from '@chakra-ui/react';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
