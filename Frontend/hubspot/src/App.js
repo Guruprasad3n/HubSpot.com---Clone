2b5d97697553fc771750fabb2ad201aac2d0d9b5
Rohit Singh <kailash.jc.rs@gmail.com>
2022-11-12 19:32:34 +0530

    minor change

--- Frontend/hubspot/src/App.js
+++ Frontend/hubspot/src/App.js
@@ -5,4 +5,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- Frontend/hubspot/src/Components/Pages/Routes.jsx
+++ Frontend/hubspot/src/Components/Pages/Routes.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import Pricing from '../Pricing/Pricing' 
+import Pricing from '../Pricing/Pricing'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function ALLRoutes() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/pricing" element = {<Pricing/>}></Route>
+          <Route path="/pricing" element={<Pricing />}></Route>
         </Routes>
-     </Router>
+      </Router>
     </div>
   )
 }
