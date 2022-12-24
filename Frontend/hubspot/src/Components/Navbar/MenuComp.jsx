3d2b2c5a90f4efb4a0d349dca01a8c93eb3260f1
Rohit Singh <kailash.jc.rs@gmail.com>
2022-12-24 10:32:12 +0530

    All issues completed

--- Frontend/hubspot/src/Components/Navbar/MenuComp.jsx
+++ Frontend/hubspot/src/Components/Navbar/MenuComp.jsx
@@ -1,6 +1,6 @@
-import { MenuButton, Menu, MenuList, MenuItem, IconButton,  Text } from '@chakra-ui/react'
+import { MenuButton, Menu, MenuList, MenuItem, IconButton, Text } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MenuComp = () => {
     return (
@@ -25,17 +25,17 @@ const MenuComp = () => {
                     </Link>
                 </MenuItem>
                 <MenuItem>
-                <Link textDecoration='none' to='/resource'>
-                <Text fontSize='1.4rem' fontWeight='500' color='#2e475d'>Resources</Text>
-                </Link>
-                </MenuItem>
-                <MenuItem>
-                    <Link textDecoration='none' to='https://help.hubspot.com/?_ga=2.23678194.978470726.1592315215-1192486389.1556035228'>
-                        <Text fontSize='1.4rem' fontWeight='500' color='#2e475d'>Customer Support</Text>
+                    <Link textDecoration='none' to='/resource'>
+                        <Text fontSize='1.4rem' fontWeight='500' color='#2e475d'>Resources</Text>
                     </Link>
                 </MenuItem>
                 <MenuItem>
-                    <Link to='https://www.hubspot.com/our-story' target='_blank'>
+                    <Link textDecoration='none' to='/signup'>
+                        <Text fontSize='1.4rem' fontWeight='500' color='#2e475d'>Sign up</Text>
+                    </Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link target='_blank'>
                         <Text fontSize='1.4rem' fontWeight='500' color='#2e475d'>About</Text>
                     </Link>
                 </MenuItem>
